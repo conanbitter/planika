1a64a43123dfafebd13e7833164fbdd216ebb4a1
Conan Bitter <conan.bitter@gmail.com>
2025-07-06 14:29:54 +0300

    Setup basic server

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,6 +1,7 @@
 /// <reference lib="dom" />
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import { StrictMode } from 'react';
 
 const root = createRoot(document.getElementById('app')!);
-root.render(<App />);
+root.render(<StrictMode><App /></StrictMode>);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import { Elysia } from "elysia";
+import { staticPlugin } from "@elysiajs/static";
+
+const app = new Elysia()
+    .use(staticPlugin())
+    .get('/favicon.ico', () => Bun.file("favicon.ico"))
+    .get('/', () => Bun.file("src/index.html"))
+    .listen(3000);
+
+console.log(`Server running at http://${app.server?.hostname}:${app.server?.port}`);
